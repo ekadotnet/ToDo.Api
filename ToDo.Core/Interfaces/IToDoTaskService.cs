ff766362856df83d34dc5ac19992f266ffaee972
Robert <robert.krzaczynski@gmail.com>
2019-05-12 12:09:15 +0200

    Add core layer - first interface and service

---
+++ ToDo.Core/Interfaces/IToDoTaskService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDo.Data.Models;
+
+namespace ToDo.Core.Interfaces
+{
+    public interface IToDoTaskService
+    {
+        void Add(ToDoTask toDoTask);
+        void Update(ToDoTask toDoTask);
+        void Delete(ToDoTask toDoTask);
+    }
+}
---
+++ ToDo.Data/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        T Add(T entity);
+        T Update(T entity);
+        T Delete(T entity);
+    }
+}
