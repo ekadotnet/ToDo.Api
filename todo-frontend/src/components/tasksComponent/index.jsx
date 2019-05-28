62a2ad6fb7b61346f1fa34a0770b20ab1e97fbb7
jakub.woszczyna <woszczyna.jakub@gmail.com>
2019-05-28 21:00:56 +0200

    Added connection to backend, getting, posting tasks, dynamic listing, css - code from 28.05 classes

--- todo-frontend/src/components/tasksComponent/index.jsx
+++ todo-frontend/src/components/tasksComponent/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Form } from "react-bootstrap";
+import axios from "axios";
+import "./style.css";
 
 export default class TasksComponent extends React.Component {
   constructor() {
@@ -11,19 +13,95 @@ export default class TasksComponent extends React.Component {
           description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ex quis, quidem enim nobis laudantium dolores, debitis eos odio cum voluptas hic possimus impedit? Obcaecati rerum aut exercitationem expedita voluptatum?"
         }
-      ]
+      ],
+      newTaskName: "",
+      newTaskDescription: ""
     };
   }
 
-  render() {
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  getTasks = () => {
+    axios.get("/toDo").then(response => {
+      console.log(response);
+      this.setState({
+        tasks: response.data
+      });
+    });
+  };
+
+  renderCard = (task, k) => {
     return (
-      <Card style={{ width: "18rem" }}>
+      <Card key={k} style={{ width: "18rem" }}>
         <Card.Body>
-          <Card.Title>{this.state.tasks[0].name}</Card.Title>
-          <Card.Text>{this.state.tasks[0].description}</Card.Text>
-          <Button variant="primary">Click</Button>
+          <Card.Title>{task.name}</Card.Title>
+          <Card.Text>{task.description}</Card.Text>
         </Card.Body>
       </Card>
+    );
+  };
+
+  addNewTask = () => {
+    axios.post("/toDo", {
+      name: this.state.newTaskName,
+      description: this.state.newTaskDescription
+    });
+    this.getTasks();
+  };
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log("tutaj:");
+    console.log(this.state);
+  };
+
+  render() {
+    let tasks = this.state.tasks
+      .slice(0)
+      .reverse()
+      .map((val, index) => {
+        return this.renderCard(val, index);
+      });
+
+    return (
+      <div id="tasksPage">
+        <div>{tasks}</div>
+        <div>
+          <Form>
+            <Form.Group>
+              <Form.Label>Task name:</Form.Label>
+              <Form.Control
+                name="newTaskName"
+                onChange={this.onChange}
+                type="text"
+                placeholder="Enter task name"
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Task description:</Form.Label>
+              <Form.Control
+                name="newTaskDescription"
+                onChange={this.onChange}
+                as="textarea"
+                rows="3"
+              />
+            </Form.Group>
+
+            <Button
+              disabled={this.state.newTaskName === ""}
+              onClick={this.addNewTask}
+              variant="primary"
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
     );
   }
 }
